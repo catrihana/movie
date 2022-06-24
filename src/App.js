9a9f220f23e0026424ab4b3f146fddb749455e83
Cindy Atrihana <cindy.atrihana@gmail.com>
2022-06-24 19:52:16 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import './style/landingPage.css';
+import NavigationBar from "./components/NavigationBar"
+import Intro from './components/Intro';
+import Trending from './components/Trending';
+import SuperHero from './components/Superhero';
+
+function App() {
+  return (
+    <div>
+      {/* intro section */}
+      <div className='myBG'>
+        <NavigationBar />
+        <Intro />
+      </div>
+      {/* end of intro */}
+
+      {/* trending section */}
+      <div className='trending'>
+        <Trending/>
+      </div>
+      {/* end of trending section */}
+
+      {/* superhero section */}
+      <div className='superhero'>
+        <SuperHero/>
+      </div>
+      {/* end of superhero section */}
+   </div>
+  )
+}
+
+export default App;
